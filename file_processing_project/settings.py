0c34ef2ef334e04c23576b8a6588de379d31b009
sanfit <94311113+sanfouse@users.noreply.github.com>
2024-02-10 02:12:57 +0300

    Update settings.py

--- file_processing_project/settings.py
+++ file_processing_project/settings.py
@@ -125,7 +125,7 @@ MAX_FILE_SIZE = 10 * 1024 * 1024
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-UPLOAD_ROOT = '/app/uploads/'
+UPLOAD_ROOT = os.path.join(BASE_DIR, "uploads")
 UPLOAD_URL = '/uploads/'
 
 STATIC_URL = "/static/"
