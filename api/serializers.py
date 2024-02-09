7a7f08c7bd762465f573c47453459ce8382f43f4
sanfit <sanfit@vk.com>
2024-02-10 00:56:15 +0300

    v1

--- api/serializers.py
+++ api/serializers.py
@@ -4,4 +4,4 @@ from .models import File
 class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
-        fields = ('id', 'file', 'uploaded_at', 'processed')
+        fields = ('__all__')
--- api/views.py
+++ api/views.py
@@ -6,27 +6,20 @@ from .models import File
 from .serializers import FileSerializer
 from .tasks import process_uploaded_file
 from rest_framework import generics
-from file_processing_project.settings import MAX_FILE_SIZE
 
 @api_view(['POST'])
 @parser_classes([MultiPartParser, FormParser])
 def upload_file(request):
     file = request.data.get('file')
-
-    if not file:
-        return Response({'error': 'No file provided.'}, status=status.HTTP_400_BAD_REQUEST)
-
-    if file.size > MAX_FILE_SIZE:
-        return Response({'error': 'File size exceeds the limit of 10 MB.'}, status=status.HTTP_400_BAD_REQUEST)
-
+    
     serializer = FileSerializer(data={'file': file})
     if serializer.is_valid():
         file_instance: File = serializer.save()
         process_uploaded_file.delay(file_instance.id)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)   
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
 
 class FileListApiView(generics.ListAPIView):
     queryset = File.objects.all()
