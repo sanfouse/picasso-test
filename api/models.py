9876a85308ac7513216596874490024293d15d1c
sanfit <sanfit@vk.com>
2024-02-11 19:03:48 +0300

    update statuses

--- api/models.py
+++ api/models.py
@@ -5,4 +5,4 @@ class File(models.Model):
     file = models.FileField(upload_to='uploads/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
     processed = models.BooleanField(default=False)
-    status_code = models.TextField(blank=True, null=True, default='200')
+    status_code = models.IntegerField(blank=True, null=True, default=400)
--- api/tasks.py
+++ api/tasks.py
@@ -2,14 +2,14 @@ from celery import shared_task
 from .models import File
 from file_processing_project.settings import MAX_FILE_SIZE
 from django.core.files.storage import default_storage
-
+from rest_framework import status
 
 @shared_task
 def process_uploaded_file(file_id):
-    status_code = '201'
+    status_code = status.HTTP_201_CREATED
     file_instance = File.objects.get(id=file_id)
 
     if default_storage.size(file_instance.file.name) > MAX_FILE_SIZE:
-        status_code = '400'
+        status_code = status.HTTP_413_REQUEST_ENTITY_TOO_LARGE
 
     File.objects.filter(id=file_id).update(processed=True, status_code=status_code)
